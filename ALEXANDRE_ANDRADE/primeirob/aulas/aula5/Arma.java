4e3f8fdf499e1248029777bdf8aceb32a182dcec
Alexandre de Andrade <aareis@minha.fag.edu.br>
2024-09-03 20:01:27 -0300

    commit bololo hahaha

---
+++ ALEXANDRE_ANDRADE/primeirob/aulas/aula5/Arma.java
@@ -0,0 +1,10 @@
+package primeirob.aulas.aula5;
+
+public class Arma {
+    String nome;
+
+    String tipo;
+
+    String dano;
+
+}
---
+++ ALEXANDRE_ANDRADE/primeirob/aulas/aula5/armaExemplo.java
@@ -0,0 +1,12 @@
+import primeirob.aulas.aula5.Arma;
+
+Arma armaExemplo = new Arma();
+
+armaExemplo.nome = "Fever Dream"
+
+armaExemplo.tipo = "AWP";
+
+armaExemplo.dano = 110;
+
+public void main() {
+}
